70cf9117a4af158eabd0b4bbbcd978f10b5b74d2
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-04-30 17:11:08 +0700

    fix profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,15 +1,31 @@
-import { makeStyles, Paper } from '@material-ui/core';
+import { Button, makeStyles, Paper } from '@material-ui/core';
+import { Edit } from '@material-ui/icons';
 import React, { useContext } from 'react'
 import UserContext from '../contexts/UserContext'
 
 const useStyles = makeStyles(theme => ({
     details: {
         padding: theme.spacing(3),
-        
     },
-
     container: {
         textAlign: 'left'
+    },
+    header: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    editButton: {
+        textTransform: 'none',
+        fontWeight: 'bold',
+    },
+
+    detailsSection: {
+        borderBottom: `2px solid ${theme.palette.divider}`,
+        paddingBottom: theme.spacing(1)
+    },
+    detailsSectionTitle: {
+        margin: 0
     }
 }))
 
@@ -23,9 +39,21 @@ export default function Profile() {
             {
                 user &&
                 <div className={classes.container}>
-                    <h1>{user.name}</h1>
+                    <div className={classes.header}>
+                        <h1>{user.name}</h1>
+                        <Button 
+                            variant="contained" 
+                            color="primary"
+                            className={classes.editButton}
+                        >
+                            Edit Profile <Edit/>
+                        </Button>
+                    </div>
                     <Paper className={classes.details}>
-                        {user.bio}
+                        <div className={classes.detailsSection}>
+                            <h2 className={classes.detailsSectionTitle}>Bio</h2>
+                            <div>{user.bio}</div>
+                        </div>
                     </Paper>
                 </div>
             }
